69368e640ae34572b2f6e9d18d37260decbc5c68
Iris <muye0928@gmail.com>
2018-12-08 21:07:26 -0500

    Update active_customers.py

--- active_customers.py
+++ active_customers.py
@@ -11,14 +11,14 @@ result = result[["Date","Email","full name"]]
 result = result.drop_duplicates(subset = "Email")
 result.head()
 
-# data cleaning for bookings dat, create a column to show month-year and make sure those data are completed bookings
+# data cleaning for bookings data
+# make sure those data are completed bookings
 df2["Date"] = df2["Appt Date"] +" " + df2["Start Time"]
 df2["Date"] = pd.to_datetime(df2["Date"])
-df2["year-month"] = df2["Date"].apply(lambda x: str(x.year) +"-"+ str(x.month))
-df2 = df2[["Full Name","Date","Status","year-month"]]
+df2 = df2[["Full Name","Date","Status"]]
 df2 = df2[df2["Status"] == "done"]
 
-index_month = pd.date_range('1/1/2017',periods = 24, freq = "M")
+index_month = pd.date_range('7/1/2017',periods = 24, freq = "M")
 def intersection(lst1, lst2): 
     return list(set(lst1) & set(lst2))
 book_month =[]

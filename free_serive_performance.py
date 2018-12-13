23d8d309b07cb5b6f61ef011bd281fddbf948859
Iris <muye0928@gmail.com>
2018-12-13 10:50:15 -0500

    Update free_serive_performance.py

--- free_serive_performance.py
+++ free_serive_performance.py
@@ -1,13 +1,15 @@
 """this task is to measure the free services performance, to find how many people came back and completed bookings
  and how many people came back no matter their bookings  status"""
 import pandas as pd
-df2 = pd.read_csv("bookings1125.csv")
+import pandas as pd
+df2 = pd.read_csv("bookings1212.csv")
 df2["Date"] = df2["Appt Date"] +" " + df2["Start Time"]
 df2["Date"] = pd.to_datetime(df2["Date"])
-df2["year-month"] = df2["Date"].apply(lambda x: str(x.year) +"-"+ str(x.month))
 df2 = df2[["Date","Full Name","Status","Full \nDiscounted"]]
+df["year-month"] = df["Appt Date"].apply(lambda x: str(x.year) +"-"+ str(x.month))
 ##those are people who use the free services
 full = df2[df2["Full \nDiscounted"] == True]
+full = full[full["Status"] =="done"]
 ## drop the duplicated names for the one who used free services
 name = full.drop_duplicates(subset="Full Name")
 ##use left join to get the result
@@ -16,7 +18,6 @@ result = pd.merge(df2,name[["Full Name", "year-month", "Date"]],on="Full Name", 
 result1 = result[result["Date_x"] > result["Date_y"]]
 # get the monthly data
 result1.groupby(['year-month_x']).size()
-
 # to find out the how many completed bookings came from people who used the free services before
 result2 = result1[result1["Status"] == "done"]
 result2.groupby(['year-month_x']).size()

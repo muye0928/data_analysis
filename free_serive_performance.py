572f0c31de09da20269e1f64af250cf8eed0ab41
Iris <muye0928@gmail.com>
2019-01-21 12:48:33 -0500

    Update free_serive_performance.py

--- free_serive_performance.py
+++ free_serive_performance.py
@@ -1,23 +1,22 @@
 """this task is to measure the free services performance, to find how many people came back and completed bookings
  and how many people came back no matter their bookings  status"""
 import pandas as pd
-import pandas as pd
-df2 = pd.read_csv("bookings1212.csv")
-df2["Date"] = df2["Appt Date"] +" " + df2["Start Time"]
-df2["Date"] = pd.to_datetime(df2["Date"])
-df2 = df2[["Date","Full Name","Status","Full \nDiscounted"]]
-df["year-month"] = df["Appt Date"].apply(lambda x: str(x.year) +"-"+ str(x.month))
+df = pd.read_csv("bookings0113.csv")
+df = df[["Full Name","Free","Appt Date","Start Time","Status"]]
+df["Date"] = df["Appt Date"]+" "+df["Start Time"]
+df["Appt Date"] = pd.to_datetime(df["Appt Date"])
+df["Date"]= pd.to_datetime(df["Date"])
+free = df[df["Free"]==True]
 ##those are people who use the free services
-full = df2[df2["Full \nDiscounted"] == True]
-full = full[full["Status"] =="done"]
-## drop the duplicated names for the one who used free services
-name = full.drop_duplicates(subset="Full Name")
-##use left join to get the result
-result = pd.merge(df2,name[["Full Name", "year-month", "Date"]],on="Full Name", how='left')
+week1 = free[(free["Appt Date"]>="2019/01/07")&(free["Appt Date"]<="2019/01/13")]
+len(set(week1["Full Name"])) # unique name
+# all status 
+result = pd.merge(df,free[["Full Name", "Date"]],on="Full Name", how='left')
 # make sure the people used the free services first and then come back
 result1 = result[result["Date_x"] > result["Date_y"]]
-# get the monthly data
-result1.groupby(['year-month_x']).size()
-# to find out the how many completed bookings came from people who used the free services before
-result2 = result1[result1["Status"] == "done"]
-result2.groupby(['year-month_x']).size()
+# how many bookings came from those who used free services
+week = result1[(result1["Appt Date"]>="2019/01/07")&(result1["Appt Date"]<="2019/01/13")]
+len(week)
+# how many completed bookings came from those used free services
+week_done = week[week["Status"]=="done"]
+len(week_done)
